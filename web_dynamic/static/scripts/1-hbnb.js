e3a30590a6a71bc32402752e8424b2407d4f98e5
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-11-03 21:09:29 +0100

    task 2

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-	let amenities = []
-	if $('#checkbox_amenities').is(':checked')) {
-		
-		$('#list_amenities').text(checked.join(',');)
-
-
+$(document).ready(function () {
+  let amenities = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+			let id = $(this).data('id');
+			let name = $(this).data('name');
+      amenities[id] = name;
+    } else {
+			let id = $(this).data('id');
+      delete amenities[id];
+    }
+    let selectedAmenities = Object.values(amenities).join(', ');
+    $('.amenities h4').text(selectedAmenities);
+  });
+});
